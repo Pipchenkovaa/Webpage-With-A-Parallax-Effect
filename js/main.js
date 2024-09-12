494f031ad95445cd09ca4da6441c2d8224392cda
Shlaampe <piphencova@icloud.com>
2024-09-12 14:38:38 +0300

    Changed styles

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@ window.addEventListener('scroll', e => {
     document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother) //регистрация плагина
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.content',
+  smooth: 1,
+  smoothTouch: 0.1,
+  effects: true,
+});
+
+if (window.innerWidth <= 768) {
+  ScrollSmoother.create({
     wrapper: '.wrapper',
-    content: '.content'
-})
+    content: '.content',
+    smooth: 0.5,
+    smoothTouch: 0.1,
+  });
+}
