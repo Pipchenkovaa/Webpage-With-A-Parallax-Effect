a1713f9bc7a0a46bf06cced9a01faa0a1ad6253c
Shlaampe <piphencova@icloud.com>
2024-09-13 11:26:23 +0300

    Changed styles

--- js/main.js
+++ js/main.js
@@ -11,11 +11,29 @@ ScrollSmoother.create({
   effects: true,
 });
 
-if (window.innerWidth <= 768) {
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+// ScrollSmoother.create({
+//   wrapper: '.wrapper',
+//   content: '.content',
+//   smooth: 1,
+//   smoothTouch: 0.1,
+//   normalizeScroll: false,
+//   effects: true,
+// });
+
+
+if (window.innerWidth > 768) {
   ScrollSmoother.create({
     wrapper: '.wrapper',
     content: '.content',
-    smooth: 0.5,
-    smoothTouch: 0.1,
+    smooth: 1,
+    effects: true,
+  });
+} else {
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 0,
   });
 }
+
