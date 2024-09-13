a6a95496f3823da738675931cbe40d09f87f427a
Shlaampe <piphencova@icloud.com>
2024-09-13 12:05:26 +0300

    Changed gsap.js files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 window.addEventListener('scroll', e => {
-    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+	document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
+
+window.addEventListener('touchstart', () => {
+	ScrollTrigger.refresh();
+});
 
 // gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 // ScrollSmoother.create({
@@ -12,12 +16,13 @@ window.addEventListener('scroll', e => {
 // });
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 ScrollSmoother.create({
   wrapper: '.wrapper',
   content: '.content',
   smooth: 1,
-  smoothTouch: 0.5,
+  smoothTouch: 0.1,
+	effects: true,
   ignoreMobileResize: true,
-  effects: true,
+  normalizeScroll: false,
 });
-
