46d6b14ce4f6a5e6d047954f6f8468c4a53cdd91
Shlaampe <piphencova@icloud.com>
2024-09-13 12:14:26 +0300

    Added adjustments in .html & .js

--- js/main.js
+++ js/main.js
@@ -6,17 +6,7 @@ window.addEventListener('touchstart', () => {
 	ScrollTrigger.refresh();
 });
 
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-// ScrollSmoother.create({
-//   wrapper: '.wrapper',
-//   content: '.content',
-//   smooth: 1,
-//   smoothTouch: 0.1,
-//   effects: true,
-// });
-
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
 ScrollSmoother.create({
   wrapper: '.wrapper',
   content: '.content',
