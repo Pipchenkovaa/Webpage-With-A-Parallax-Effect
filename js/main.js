28874f317c12abbe1488bdc62b575ef45c8154ce
Shlaampe <piphencova@icloud.com>
2024-09-13 11:33:08 +0300

    Changed js script

--- js/main.js
+++ js/main.js
@@ -2,38 +2,22 @@ window.addEventListener('scroll', e => {
     document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content',
-  smooth: 1,
-  smoothTouch: 0.1,
-  effects: true,
-});
-
 // gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 // ScrollSmoother.create({
 //   wrapper: '.wrapper',
 //   content: '.content',
 //   smooth: 1,
 //   smoothTouch: 0.1,
-//   normalizeScroll: false,
 //   effects: true,
 // });
 
-
-if (window.innerWidth > 768) {
-  ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content',
-    smooth: 1,
-    effects: true,
-  });
-} else {
-  ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content',
-    smooth: 0,
-  });
-}
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.content',
+  smooth: 1,
+  smoothTouch: 0.5,
+  ignoreMobileResize: true,
+  effects: true,
+});
 
